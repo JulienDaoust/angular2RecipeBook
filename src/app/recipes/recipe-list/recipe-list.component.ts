dfb01084ed4714153990fc270df29a12ebf51ffe
julienDaoustCistec <jdaoust@cistec.ca>
2017-02-06 13:40:31 -0500

    Directives

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,11 @@ import { Recipe } from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+      new Recipe('Poutine', 'Tres graisseuse', 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/8/29/1/FNM_100111-Poutine-002_s4x3.jpg.rend.sni18col.jpeg', []),
+      new Recipe('Salade Verte', 'Tres gouteuse', 'http://www.911pizza.fr/img/products/3958a8b2606deed7b1b9dd412af56000-w380.png', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://www.planete-gateau.com/11464-large_default/dummy-rond-polystyrene-diametre-15cm-hauteur-10cm.jpg');
 
   constructor() { }
 
