3b467f014d61bd91752d755939e7444fba176557
julienDaoustCistec <jdaoust@cistec.ca>
2017-02-17 10:16:20 -0500

    edit, adding et clearing items

--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
 
 import { Ingredient } from "../shared/ingredient";
 import { ShoppingListService } from "./shopping-list.service";
@@ -7,20 +7,38 @@ import { ShoppingListService } from "./shopping-list.service";
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
-export class ShoppingListAddComponent implements OnInit {
+export class ShoppingListAddComponent implements OnChanges {
   isAdd = true;
-  item: Ingredient;
+  @Input() item: Ingredient;
+  @Output() cleared = new EventEmitter(); 
   constructor(private sls: ShoppingListService) { }
 
-  ngOnInit() {
+  ngOnChanges(changes) {
+    if (changes.item.currentValue === null) {
+      this.isAdd = true;
+      this.item = {name: null, amount: null};
+    } else {
+      this.isAdd = false;
+    }
   }
 
   onSubmit(ingredient: Ingredient) {
+    const newIngredient = new Ingredient(ingredient.name, ingredient.amount);
     if (!this.isAdd) {
-      // Edit
+      this.sls.editItem(this.item, new Ingredient(ingredient.name, ingredient.amount));
+      this.onClear();
     } else {
-      this.item = new Ingredient(ingredient.name, ingredient.amount);
+      this.item = newIngredient
       this.sls.addItem(this.item);
     }
+  }
+
+  onDelete() {
+    this.sls.deleteItem(this.item);
+  }
+
+  onClear() {
+    this.isAdd =true;
+    this.cleared.emit(null);
   }
 }
