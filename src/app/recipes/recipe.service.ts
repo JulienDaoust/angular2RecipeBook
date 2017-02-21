847a8962f3e5dece9c95026b3c889bcf8fecf2f2
julienDaoustCistec <jdaoust@cistec.ca>
2017-02-21 09:59:22 -0500

    fetch data server firebase

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
 
 import { Recipe } from './recipe';
 import { Ingredient } from '../shared/ingredient';
+import 'rxjs/Rx';
 
 @Injectable()
 export class RecipeService {
-private recipes: Recipe[] = [
-      new Recipe('Poutine', 'Tres graisseuse', 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/8/29/1/FNM_100111-Poutine-002_s4x3.jpg.rend.sni18col.jpeg', [
-        new Ingredient('Patates frites', 15),
-        new Ingredient('Fromage en crottes', 10)
-      ]),
-      new Recipe('Salade Verte', 'Tres gouteuse', 'http://www.911pizza.fr/img/products/3958a8b2606deed7b1b9dd412af56000-w380.png', [
-        new Ingredient('Pomme de salade', 1),
-        new Ingredient('Croutons', 5),
-        new Ingredient('Epinads', 5)
-      ])
+  recipesChanged = new EventEmitter<Recipe[]>();
+
+  private recipes: Recipe[] = [
+    new Recipe('Poutine', 'Tres graisseuse', 'http://foodnetwork.sndimg.com/content/dam/images/food/fullset/2011/8/29/1/FNM_100111-Poutine-002_s4x3.jpg.rend.sni18col.jpeg', [
+      new Ingredient('Patates frites', 15),
+      new Ingredient('Fromage en crottes', 10)
+    ]),
+    new Recipe('Salade Verte', 'Tres gouteuse', 'http://www.911pizza.fr/img/products/3958a8b2606deed7b1b9dd412af56000-w380.png', [
+      new Ingredient('Pomme de salade', 1),
+      new Ingredient('Croutons', 5),
+      new Ingredient('Epinads', 5)
+    ])
   ];
-  constructor(private http: Http) {}
+  constructor(private http: Http) { }
 
   getRecipes() {
     return this.recipes;
@@ -28,10 +31,10 @@ private recipes: Recipe[] = [
   }
 
   deleteRecipe(recipe: Recipe) {
-    this.recipes.splice(this.recipes.indexOf(recipe),1);
+    this.recipes.splice(this.recipes.indexOf(recipe), 1);
   }
 
-  addRecipe(recipe: Recipe){
+  addRecipe(recipe: Recipe) {
     this.recipes.push(recipe);
   }
 
@@ -44,10 +47,17 @@ private recipes: Recipe[] = [
     const headers = new Headers({
       'Content-Type': 'application/json'
     });
-    return this.http.post('https://recipebook-f948a.firebaseio.com/recipes.json', body, {headers: headers})
+    return this.http.put('https://recipebook-f948a.firebaseio.com/recipes.json', body, { headers: headers })
   }
 
   fetchData() {
-
+    return this.http.get('https://recipebook-f948a.firebaseio.com/recipes.json')
+      .map((response: Response) => response.json())
+      .subscribe(
+      (data: Recipe[]) => {
+        this.recipes = data;
+        this.recipesChanged.emit(this.recipes);
+      }
+      );
   }
 }
